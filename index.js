cda717b572bb7bbf6973094dfe22190beb19534c
Mahary R <maharyfenitra@gmail.com>
2022-12-29 15:33:23 +0300

    Mongoose setup

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ app.use("/management",managementRoutes)
 app.use("sales",salesRoutes)
 
 /**Mogoose Setup */
+const PORT = process.env.PORT||5001;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server Port ${PORT}`)
+    })
+}).catch((error) => console.log(`${error} did not connect`))
